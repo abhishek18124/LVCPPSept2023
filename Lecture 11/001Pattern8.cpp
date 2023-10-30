fab135125b269c208bd5f1f985475e84d6f0bbec
abhishek18124 <abhishek18124@iiitd.ac.in>
2023-10-30 20:17:32 +0530

    Lecture 11

---
+++ Lecture 11/001Pattern8.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 5;
+
+	for (int i = 1; i <= n; i++) {
+
+		// for the ith row, print n-i+1 characters in the increasing order starting with 'A'
+
+		char ch = 'A';
+
+		for (int j = 1; j <= n - i + 1; j++) {
+
+			cout << ch << " ";
+			ch++; // ch = ch + 1
+
+		}
+
+		cout << endl;
+
+	}
+
+	return 0;
+}
