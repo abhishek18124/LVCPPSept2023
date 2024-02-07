5219a1cd44a2088a2eaa2598f390f5eb75e222d2
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-02-07 21:14:15 +0530

    Lecture 48

--- Lecture 48/001LinkedList_RemoveCycle.cpp
+++ Lecture 48/001LinkedList_RemoveCycle.cpp
@@ -18,20 +18,50 @@ public:
 void insertAtHead(ListNode*& head, int val) {
 
 	ListNode* n = new ListNode(val);
-	n->next = head; 
+	n->next = head;
 	head = n;
 
 }
 
 
 void printLinkedList(ListNode* head) {
-	
-	while(head) { // head != NULL
+
+	while (head) { // head != NULL
 		cout << head->val << " ";
 		head = head->next;
 	}
 
 	cout << endl;
+}
+
+void removeCycle(ListNode* head) {
+
+	ListNode* slow = head;
+	ListNode* fast = head;
+
+	while (true) {
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast) {
+			// slow and fast pointers are at the meeting point
+			break;
+		}
+	}
+
+	slow = head;
+
+	while (slow->next != fast->next) {
+		slow = slow->next;
+		fast = fast->next;
+	}
+
+	// at this point of time, slow->next and fast->next are pointing to the start of the cycle
+
+	// therefore, the fast pointer is pointing to the tail node of the LinkedList
+
+	fast->next = NULL;
+
+
 }
 
 int main() {
@@ -43,8 +73,10 @@ int main() {
 	head->next->next->next->next = new ListNode(50);
 	head->next->next->next->next->next = new ListNode(60);
 	head->next->next->next->next->next->next = head->next;
-	
-	// todo ...
-	 	
+
+	removeCycle(head);
+
+	printLinkedList(head);
+
 	return 0;
 }
--- Lecture 48/003LinkedList_MergeSort.cpp
+++ Lecture 48/003LinkedList_MergeSort.cpp
@@ -18,15 +18,15 @@ public:
 void insertAtHead(ListNode*& head, int val) {
 
 	ListNode* n = new ListNode(val);
-	n->next = head; 
+	n->next = head;
 	head = n;
 
 }
 
 
 void printLinkedList(ListNode* head) {
-	
-	while(head) { // head != NULL
+
+	while (head) { // head != NULL
 		cout << head->val << " ";
 		head = head->next;
 	}
@@ -34,22 +34,92 @@ void printLinkedList(ListNode* head) {
 	cout << endl;
 }
 
-ListNode* getMidPoint(ListNode* head) {
-	if(!head) { // head == NULL
-		return head;
+// time : O(n)
+// space: O(1)
+
+ListNode* computeMidPoint(ListNode* head) {
+
+	if (head == NULL) {
+		// LinkedList is empty
+		return NULL;
 	}
 
 	ListNode* slow = head;
 	ListNode* fast = head->next;
 
-	while(fast != NULL && fast->next != NULL) {
+	while (fast != NULL and fast->next != NULL) {
 		slow = slow->next;
 		fast = fast->next->next;
 	}
 
+	// slow is pointing to the midPoint of the LinkedList i.e. slow pointer has the address of the midPoint node
 	return slow;
+
 }
 
+// time : O(n)
+// space: O(1)
+
+ListNode* mergeSortedLinkedList(ListNode* head1, ListNode* head2) {
+
+	ListNode* dummy = new ListNode(0); // we are using 0 as a dummy value
+	ListNode* temp = dummy;
+
+	while (head1 != NULL and head2 != NULL) {
+
+		if (head1->val < head2->val) {
+			temp->next = head1;
+			temp = temp->next;
+			head1 = head1->next;
+		} else {
+			temp->next = head2;
+			temp = temp->next;
+			head2 = head2->next;
+		}
+
+	}
+
+	if (head1 != NULL) {
+		temp->next = head1;
+	}
+
+	if (head2 != NULL) {
+		temp->next = head2;
+	}
+
+	return dummy->next;
+
+
+
+}
+
+// time : O(nlogn)
+// space: O(logn) due to the function call stack
+
+ListNode* mergeSort(ListNode* head) {
+
+	// base case
+	if (head == NULL || head->next == NULL) {
+		// LinkedList is empty or LinkedList has one node
+		return head;
+	}
+
+	// recursive case
+
+	// 1. divide the LinkedList around the midPoint
+
+	ListNode* midPoint = computeMidPoint(head);
+	ListNode* head2 = midPoint->next;
+	midPoint->next = NULL;
+
+	// 2. recursively, sort the two sublists
+	head = mergeSort(head);
+	head2 = mergeSort(head2);
+
+	// 3. merge the two sorted sublists
+	return mergeSortedLinkedList(head, head2);
+
+}
 
 int main() {
 
@@ -63,7 +133,7 @@ int main() {
 
 	printLinkedList(head);
 
-	// todo ...
+	head = mergeSort(head);
 
 	printLinkedList(head);
 
