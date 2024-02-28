7c67588afefd163420b1528b8275529431526f05
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-02-28 18:55:43 +0530

    Lecture 59

---
+++ Lecture 59/001Heaps_Implementation.cpp
@@ -0,0 +1,40 @@
+/*
+
+	Implementation of the heap/priority_queue data structure.
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	minHeap m;
+
+	m.push(9);
+	m.push(7);
+	m.push(8);
+	m.push(5);
+	m.push(4);
+	m.push(6);
+	m.push(3);
+	m.push(2);
+	m.push(1);
+
+	cout << m.size() << endl;
+
+	while(!m.empty()) {
+		cout << m.top() << " ";
+		m.pop();
+	}
+
+	cout << endl;
+
+	cout << m.size() << endl;
+
+	return 0;
+}
+
+
+
---
+++ Lecture 59/005Heaps_BuildHeap.cpp
@@ -0,0 +1,30 @@
+/*
+
+	Given an array of n integers, design a O(n) algorirthm to build a max_heap in-place.
+
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	vector<int> v = {1, 3, 2, 7, 5, 4, 6, 9, 8};
+	
+	// todo ...
+
+	for(int i=0; i<v.size(); i++) {
+		cout << v[i] << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
+
+
+
+
+
