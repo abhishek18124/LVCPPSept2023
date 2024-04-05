9f575704707d7bfc00970855495f7ef922e161a3
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-04-05 18:51:09 +0530

    Lecture 73

---
+++ Lecture 74/005Graphs_SSSPUsingBFS.cpp
@@ -0,0 +1,85 @@
+/*
+
+Computing SSSP using the BFS algorithm in an unweighted graph.
+
+Note : each vertex in the graph is generic.
+
+*/
+
+#include<iostream>
+#include<map>
+#include<list>
+#include<unordered_set>
+#include<queue>
+
+using namespace std;
+
+template <typename T>
+class graph {
+
+	map<T, list<T>> neighbourMap; // to store the graph representation
+	bool isDirected; // to indicate if the graph is directed or not
+
+public :
+
+	graph(bool isDirected = false) {
+		this->isDirected = isDirected;
+	}
+
+	void addEdge(T u, T v) {
+
+		// adds an edge b/w vertex u and v
+
+		neighbourMap[u].push_back(v);
+
+		if (!isDirected) {
+			neighbourMap[v].push_back(u);
+		}
+
+	}
+
+	void bfs(T s, T d) {
+
+		map<T, int> distMap; // to store the mapping between vertices &
+		// their shortest dist from the src vertex
+		distMap[s] = 0;
+
+		unordered_set<T> visited; // to keep track of visited vertices
+		queue<T> q; // to keep track of visited & un-explored vertices
+
+		// mark 's' as visited
+		visited.insert(s);
+		q.push(s);
+
+		while (!q.empty()) {
+			T front = q.front();
+			q.pop();
+
+			// explore 'front'
+
+			// todo ...
+
+		}
+	}
+};
+
+
+int main() {
+
+	graph<char> g;
+
+	g.addEdge('A', 'B');
+	g.addEdge('A', 'C');
+	g.addEdge('B', 'D');
+	g.addEdge('B', 'E');
+	g.addEdge('C', 'E');
+	g.addEdge('C', 'F');
+	g.addEdge('D', 'G');
+	g.addEdge('E', 'G');
+	g.addEdge('E', 'H');
+	g.addEdge('F', 'H');
+	g.addEdge('G', 'I');
+	g.addEdge('H', 'I');
+
+	return 0;
+}
