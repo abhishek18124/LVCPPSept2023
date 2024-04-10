0cfdd332e587e847265fa8c1e8cfa5a76ca6ab16
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-04-10 21:10:56 +0530

    Lecture 76

--- Lecture 76/001Graphs_01Matrix.cpp
+++ Lecture 76/001Graphs_01Matrix.cpp
@@ -2,6 +2,7 @@
 
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<climits>
 
 using namespace std;
@@ -9,18 +10,19 @@ using namespace std;
 // 1. brute-force approach - time : O(m^2.n^2)
 
 vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-    
+
 	int m = mat.size();
-	int n = mat[0].size(); 
+	int n = mat[0].size();
 
 	vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
 
-	for(int i=0; i<m; i++) {
-		for(int j=0; j<n; j++) {
-			if(mat[i][j] == 0) {
-				for(int ii=0; ii<m; ii++) {
-					for(int jj=0; jj<n; jj++) {
-						dist[ii][jj] = min(dist[ii][jj], abs(ii-i)+abs(jj-j));
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (mat[i][j] == 0) {
+				// cal. the dist of all the cells in mat[][] from the (i, j)th cell
+				for (int ii = 0; ii < m; ii++) {
+					for (int jj = 0; jj < n; jj++) {
+						dist[ii][jj] = min(dist[ii][jj], abs(ii - i) + abs(jj - j));
 					}
 				}
 			}
@@ -31,18 +33,83 @@ vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
 
 }
 
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
+
+vector<vector<int>> updateMatrixOptimised(vector<vector<int>>& mat) {
+
+	int m = mat.size();
+	int n = mat[0].size();
+
+	vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+
+	queue<pair<int, int>> q;
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (mat[i][j] == 0) {
+				// (i, j)th cell corr. to a source vertex
+				dist[i][j] = 0;
+				q.push({i, j});
+			}
+		}
+	}
+
+	while (!q.empty()) {
+
+		pair<int, int> front = q.front();
+		q.pop();
+
+		int i = front.first;
+		int j = front.second;
+
+		for (int k = 0; k < 4; k++) {
+
+			int ii = i + dx[k];
+			int jj = j + dy[k];
+
+			if (ii >= 0 and ii < m and jj >= 0 and jj < n and dist[ii][jj] == INT_MAX) {
+
+				// (ii, jj)th cell is not yet visited
+
+				q.push({ii, jj});
+				dist[ii][jj] = dist[i][j] + 1;
+
+			}
+
+		}
+
+	}
+
+	return dist;
+
+}
+
 
 int main() {
 
-	vector<vector<int>> mat = {{0, 1, 1, 1, 0}, 
-                               {1, 1, 1, 1, 1}, 
-                               {1, 1, 0, 1, 1}, 
-                               {1, 1, 1, 1, 1},
-                               {0, 1, 1, 1, 0}};
+	vector<vector<int>> mat = {{0, 1, 1, 1, 0},
+		{1, 1, 1, 1, 1},
+		{1, 1, 0, 1, 1},
+		{1, 1, 1, 1, 1},
+		{0, 1, 1, 1, 0}
+	};
 
 	vector<vector<int>> dist = updateMatrix(mat);
-	for(vector<int> row : dist) {
-		for(int col : row) {
+
+	for (vector<int> row : dist) {
+		for (int col : row) {
+			cout << col << " ";
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+
+	dist = updateMatrixOptimised(mat);
+
+	for (vector<int> row : dist) {
+		for (int col : row) {
 			cout << col << " ";
 		}
 		cout << endl;
--- Lecture 76/003Graphs_SSSPUsingDijkstra.cpp
+++ Lecture 76/003Graphs_SSSPUsingDijkstra.cpp
@@ -21,91 +21,98 @@ class graph {
 	map<T, list<pair<T, int>>> neighbourMap; // to store the graph representation
 	bool isDirected; // to indicate if the graph is directed or not
 
-	public :
+public :
 
-		graph(bool isDirected=false) {
-			this->isDirected = isDirected;
+	graph(bool isDirected = false) {
+		this->isDirected = isDirected;
+	}
+
+	void addEdge(T u, T v, int w) {
+		// add a weighted-edge b/w u and v
+		neighbourMap[u].push_back({v, w});
+		if (!isDirected) {
+			neighbourMap[v].push_back({u, w});
 		}
+	}
 
-		void addEdge(T u, T v, int w) {
-			// add a weighted-edge b/w u and v 
-			neighbourMap[u].push_back({v, w});
-			if(!isDirected) {
-				neighbourMap[v].push_back({u, w});
-			}
-		}
-
-		void print() {
-			for(pair<T, list<pair<T, int>>> vertex : neighbourMap) {
-				T vertexLabel = vertex.first;
-				list<pair<T, int>> neighbourList = vertex.second;
-				cout << vertexLabel << " : ";
-				for(pair<T, int> neighbour : neighbourList) {
-					T neighbourLabel = neighbour.first;
-					int edgeWeight = neighbour.second;
-					cout << "(" << neighbourLabel << ", " << edgeWeight << ") ";
-				}
-				cout << endl;
+	void print() {
+		for (pair<T, list<pair<T, int>>> vertex : neighbourMap) {
+			T vertexLabel = vertex.first;
+			list<pair<T, int>> neighbourList = vertex.second;
+			cout << vertexLabel << " : ";
+			for (pair<T, int> neighbour : neighbourList) {
+				T neighbourLabel = neighbour.first;
+				int edgeWeight = neighbour.second;
+				cout << "(" << neighbourLabel << ", " << edgeWeight << ") ";
 			}
 			cout << endl;
 		}
+		cout << endl;
+	}
 
-		void dijkstra(T s) {
+	void dijkstra(T s) {
 
-			map<T, int> distMap; // to store a mapping b/w vertices and their
-			                     // shortest distances from the source vertex
-			for(pair<T, list<pair<T, int>>> vertex: neighbourMap) {
-				T vertexLabel = vertex.first;
-				distMap[vertexLabel] = INT_MAX;
-			}
-			distMap[s] = 0;
-
-			unordered_set<T> explored; // to keep track of explored vertices
-
-			set<pair<int, T>> minHeap; // to keep track of unexplored vertices
-			for(pair<T, int> vertex : distMap) {
-				T vertexLabel = vertex.first;
-				int vertexDist = vertex.second;
-				minHeap.insert({vertexDist, vertexLabel});
-			}
-
-			while(!minHeap.empty()) {
-
-				pair<int, T> minVertex = *minHeap.begin();
-				minHeap.erase(minHeap.begin());
-
-				int minVertexDist = minVertex.first;
-				T minVertexLabel  = minVertex.second;
-
-				list<pair<T, int>> neighbourList = neighbourMap[minVertexLabel];
-				for(pair<T, int> neighbour : neighbourList) {
-					T neighbourLabel = neighbour.first;
-					int edgeWeight = neighbour.second;
-					if(explored.find(neighbourLabel) == explored.end() && 
-					   distMap[neighbourLabel] > distMap[minVertexLabel] + edgeWeight) {
-
-						// relax the edge between the minVertexLabel and neighbourLabel
-
-						// update the distance of the neighbourLabel in the minHeap
-						auto it = minHeap.find({distMap[neighbourLabel], neighbourLabel});
-						minHeap.erase(it);
-						minHeap.insert({distMap[minVertexLabel]+edgeWeight, neighbourLabel});
-
-						// update the distance of the neighbourLabel in the distMap
-						distMap[neighbourLabel] = distMap[minVertexLabel] + edgeWeight;
-
-					}
-				}
-				explored.insert(minVertexLabel);
-			}
-
-			for(pair<T, int> vertex : distMap) {
-				T vertexLabel = vertex.first;
-				int vertexDist = vertex.second;
-				cout << vertexLabel << " " << vertexDist << endl;
-			}
-			cout << endl;
+		map<T, int> distMap; // to store a mapping b/w vertices and their
+		// shortest distances from the source vertex
+		for (pair<T, list<pair<T, int>>> p : neighbourMap) {
+			T vertexLabel = p.first;
+			distMap[vertexLabel] = INT_MAX;
 		}
+		distMap[s] = 0;
+
+		unordered_set<T> explored; // to keep track of explored vertices
+
+		set<pair<int, T>> minHeap; // to keep track of unexplored vertices
+		for (pair<T, int> p : distMap) {
+			T vertexLabel = p.first;
+			int vertexDist = p.second;
+			minHeap.insert({vertexDist, vertexLabel});
+		}
+
+		// time : VlogV (for extracting from minHeap) + ElogV (for relaxing tensed edges)
+
+		// sparse graph i.e. E ~ V therfore time = VlogV + VlogV ~ O(VlogV)
+		// dense graph i.e. E ~ V^2 therefore time = VlogV + V^2logV ~ O(V^2logV)
+
+		// space : size of minHeap (unexplored) + size of set (explored) = O(V)
+
+		while (!minHeap.empty()) {
+
+			pair<int, T> minVertex = *minHeap.begin();
+			minHeap.erase(minHeap.begin()); // logV
+
+			int minVertexDist = minVertex.first;
+			T minVertexLabel  = minVertex.second;
+
+			list<pair<T, int>> neighbourList = neighbourMap[minVertexLabel];
+			for (pair<T, int> neighbour : neighbourList) {
+				T neighbourLabel = neighbour.first;
+				int edgeWeight = neighbour.second;
+				if (explored.find(neighbourLabel) == explored.end() &&
+				        distMap[neighbourLabel] > distMap[minVertexLabel] + edgeWeight) {
+
+					// relax the edge between the minVertexLabel and neighbourLabel
+
+					// update the distance of the neighbourLabel in the minHeap // ~logV
+					auto it = minHeap.find({distMap[neighbourLabel], neighbourLabel}); // logV
+					minHeap.erase(it); // logV
+					minHeap.insert({distMap[minVertexLabel] + edgeWeight, neighbourLabel}); // logV
+
+					// update the distance of the neighbourLabel in the distMap
+					distMap[neighbourLabel] = distMap[minVertexLabel] + edgeWeight;
+
+				}
+			}
+			explored.insert(minVertexLabel);
+		}
+
+		for (pair<T, int> p : distMap) {
+			T vertexLabel = p.first;
+			int vertexDist = p.second;
+			cout << vertexLabel << " " << vertexDist << endl;
+		}
+		cout << endl;
+	}
 
 };
 
